dca0264feafb738b998687f8071b123dca51b097
sandhyaranipotta <55246151+sandhyaranipotta@users.noreply.github.com>
2019-09-12 21:35:32 +0530

    Add files via upload

---
+++ LoginModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace JWTTokenCreation.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+   
+    public class LoginResponse
+        {
+            //public LoginResponse()
+            //{
+
+            //    this.Token = "";
+            //    this.responseMessage = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.Unauthorized };
+            //}
+
+            public string Token { get; set; }
+            public HttpResponseMessage responseMessage { get; set; }
+
+        }
+
+    }
